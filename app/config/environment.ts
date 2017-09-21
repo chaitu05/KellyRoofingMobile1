0b31129c67cbdc19dd6cc1ff3265a1e1889747ab
kumar m <cmynam@gmail.com>
2017-09-21 13:57:14 -0600

    Environment setup.

--- app/config/environment.ts
+++ app/config/environment.ts
@@ -1,7 +1,19 @@
+/** 
+ * Please don't add to this file. 
+ * This file gets GENERATED automatically.
+ * Please add properties to config/env.common.properties or 
+ *	config/environment specific file. 
+ * Properties in env.common.properties can be overridden in environment
+ * specific file. 
+ **/
 export const environment = {
 
-    ldapServerUrl: "ldap://ldapdevvip.co.ihc.com",
-    commonDummyNum: 1,
-    commonDummyBool: false,
-    commonDummyDate: new Date(2016, 12, 31),
+	ldapServerUrl : "ldap://ldapdevvip.co.ihc.com",
+	commonDummyNum : 1,
+	commonDummyBool : false,
+	commonDummyDate : new Date(2016, 12, 31),
+	TodayOrders : "TodayOrders",
+	TomorrowOrders : "TomorrowOrders",
+	Next7DayOrders : "Next7DayOrders",
+
 }
