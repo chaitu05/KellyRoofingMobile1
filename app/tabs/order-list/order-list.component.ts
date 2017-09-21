356178472eb8f0cd50808c5d80487bcd88d51771
kumar m <cmynam@gmail.com>
2017-09-21 16:41:08 -0600

    adding scss styling to order-list.

--- app/tabs/order-list/order-list.component.ts
+++ app/tabs/order-list/order-list.component.ts
@@ -3,7 +3,8 @@ import {Component, Input, OnInit} from "@angular/core";
 @Component({
     selector: "OrderList",
     moduleId: module.id,
-    templateUrl: "./order-list.component.html"
+    templateUrl: "./order-list.component.html",
+    styleUrls: ["./order-list.component.css"]
 })
 export class OrderListComponent implements OnInit {
 
