d00d0501c2bd1539ea1f39008f05979a1a8bd170
kumar m <cmynam@gmail.com>
2017-09-21 16:53:07 -0600

    nativescript-cardview added. Dummy data is displayed on order-list.html

--- app/tabs/order-list/order-list.component.ts
+++ app/tabs/order-list/order-list.component.ts
@@ -1,4 +1,10 @@
 import {Component, Input, OnInit} from "@angular/core";
+import * as elementRegistryModule from 'nativescript-angular/element-registry';
+import {Order} from "../../model/order";
+import {ItemEventData} from "tns-core-modules/ui/list-view";
+
+elementRegistryModule.registerElement("CardView",
+    () => require("nativescript-cardview").CardView);
 
 @Component({
     selector: "OrderList",
@@ -7,6 +13,8 @@ import {Component, Input, OnInit} from "@angular/core";
     styleUrls: ["./order-list.component.css"]
 })
 export class OrderListComponent implements OnInit {
+
+    public orders: Array<Order> = [];
 
     @Input() fromDate: Date;
     @Input() toDate: Date;
@@ -25,5 +33,118 @@ export class OrderListComponent implements OnInit {
         *************************************************************/
         console.log('$$$$$$$$  in ngOnint, from date: ' + this.fromDate);
         console.log('$$$$$$$$  in ngOnint, to date: ' + this.toDate);
+        this.dummyOrderInitialize();
+    }
+
+    onOrderTap(args: ItemEventData): void {
+        console.log('$$$$$$$$$ Item tapped: ' + args);
+    }
+
+    public getOrderStatus(order:Order):string {
+
+        if (order.isShipped)
+            return "Shipped";
+        else if (!order.orderPlaced)
+            return "Not-Ordered";
+        else if (order.orderConfirmations.length == 0 && order.orderPlaced)
+            return "Ordered";
+
+        return "Confirmed " + order.orderConfirmations[0].priorDays +
+            (order.orderConfirmations[0].priorDays > 1 ? "day " : "days " + "prior");
+
+    };
+
+    private dummyOrderInitialize() {
+        this.orders.push({
+            purchOrderNum: 12345, salesOrderNum: 2789, licenseNum: 2,
+            jobName: 'wal-mart', materialType: 'ISO', orderDate: new Date(),
+            pickupDate: new Date(), city: 'Salt lake', orderPlaced: true,
+            orderConfirmations: [{confirmed: true, priorDays: 4, confirmedAt: new Date()},
+                {confirmed: false, priorDays: 1, confirmedAt: new Date()}], isShipped: false,
+        });
+        this.orders.push({
+            purchOrderNum: 12346, salesOrderNum: 3789, licenseNum: 2,
+            jobName: 'wal-mart', materialType: 'Material', orderDate: new Date(),
+            pickupDate: new Date(), city: 'Salt lake', orderPlaced: true,
+            orderConfirmations: [{confirmed: true, priorDays: 4, confirmedAt: new Date()},
+                {confirmed: true, priorDays: 1, confirmedAt: new Date()}], isShipped: false
+        });
+        this.orders.push({
+            purchOrderNum: 12347, salesOrderNum: 4789, licenseNum: 2,
+            jobName: 'wal-mart', materialType: 'Skylites', orderDate: new Date(),
+            pickupDate: new Date(), city: 'Salt lake', orderPlaced: true,
+            orderConfirmations: [{confirmed: true, priorDays: 4, confirmedAt: new Date()},
+                {confirmed: false, priorDays: 1, confirmedAt: new Date()}], isShipped: false
+        });
+        this.orders.push({
+            purchOrderNum: 12348, salesOrderNum: 5789, licenseNum: 2,
+            jobName: 'Target', materialType: 'Metal', orderDate: new Date(),
+            pickupDate: new Date(), city: 'Salt lake', orderPlaced: true,
+            orderConfirmations: [{confirmed: true, priorDays: 4, confirmedAt: new Date()},
+                {confirmed: true, priorDays: 1, confirmedAt: new Date()}], isShipped: false
+        });
+        this.orders.push({
+            purchOrderNum: 12349, salesOrderNum: 6789, licenseNum: 2,
+            jobName: 'Target', materialType: 'Material', orderDate: new Date(),
+            pickupDate: new Date(), city: 'Salt lake', orderPlaced: true,
+            orderConfirmations: [{confirmed: true, priorDays: 4, confirmedAt: new Date()},
+                {confirmed: false, priorDays: 1, confirmedAt: new Date()}], isShipped: false
+        });
+        this.orders.push({
+            purchOrderNum: 22345, salesOrderNum: 7789, licenseNum: 2,
+            jobName: 'Smiths', materialType: 'Metal', orderDate: new Date(),
+            pickupDate: new Date(), city: 'Salt lake', orderPlaced: true,
+            orderConfirmations: [{confirmed: true, priorDays: 4, confirmedAt: new Date()},
+                {confirmed: true, priorDays: 1, confirmedAt: new Date()}], isShipped: false
+        });
+        this.orders.push({
+            purchOrderNum: 22345, salesOrderNum: 8789, licenseNum: 2,
+            jobName: 'Smiths', materialType: 'Skylites', orderDate: new Date(),
+            pickupDate: new Date(), city: 'Salt lake', orderPlaced: true,
+            orderConfirmations: [{confirmed: false, priorDays: 4, confirmedAt: new Date()},
+                {confirmed: false, priorDays: 1, confirmedAt: new Date()}], isShipped: false
+        });
+        this.orders.push({
+            purchOrderNum: 22345, salesOrderNum: 9789, licenseNum: 2,
+            jobName: 'Smiths', materialType: 'ISO', orderDate: new Date(),
+            pickupDate: new Date(), city: 'Salt lake', orderPlaced: true,
+            orderConfirmations: [{confirmed: true, priorDays: 4, confirmedAt: new Date()},
+                {confirmed: true, priorDays: 1, confirmedAt: new Date()}], isShipped: true
+        });
+        this.orders.push({
+            purchOrderNum: 22345, salesOrderNum: 10789, licenseNum: 2,
+            jobName: 'Smiths', materialType: 'ISO', orderDate: new Date(),
+            pickupDate: new Date(), city: 'Salt lake', orderPlaced: false,
+            orderConfirmations: [{confirmed: false, priorDays: 4, confirmedAt: new Date()},
+                {confirmed: false, priorDays: 1, confirmedAt: new Date()}], isShipped: false
+        });
+        this.orders.push({
+            purchOrderNum: 32345, salesOrderNum: 100789, licenseNum: 2,
+            jobName: 'Sams club', materialType: 'Meta', orderDate: new Date(),
+            pickupDate: new Date(), city: 'Salt lake', orderPlaced: false,
+            orderConfirmations: [{confirmed: false, priorDays: 4, confirmedAt: new Date()},
+                {confirmed: false, priorDays: 1, confirmedAt: new Date()}], isShipped: false
+        });
+        this.orders.push({
+            purchOrderNum: 42345, salesOrderNum: 110789, licenseNum: 2,
+            jobName: 'Costco', materialType: 'Material', orderDate: new Date(),
+            pickupDate: new Date(), city: 'Salt lake', orderPlaced: false,
+            orderConfirmations: [{confirmed: false, priorDays: 4, confirmedAt: new Date()},
+                {confirmed: false, priorDays: 1, confirmedAt: new Date()}], isShipped: false
+        });
+        this.orders.push({
+            purchOrderNum: 52345, salesOrderNum: 120789, licenseNum: 2,
+            jobName: 'Costco', materialType: 'Skylites', orderDate: new Date(),
+            pickupDate: new Date(), city: 'Salt lake', orderPlaced: false,
+            orderConfirmations: [{confirmed: false, priorDays: 4, confirmedAt: new Date()},
+                {confirmed: false, priorDays: 1, confirmedAt: new Date()}], isShipped: false
+        });
+        this.orders.push({
+            purchOrderNum: 62345, salesOrderNum: 130789, licenseNum: 2,
+            jobName: 'Lees', materialType: 'ISO', orderDate: new Date(),
+            pickupDate: new Date(), city: 'Salt lake', orderPlaced: false,
+            orderConfirmations: [{confirmed: false, priorDays: 4, confirmedAt: new Date()},
+                {confirmed: false, priorDays: 1, confirmedAt: new Date()}], isShipped: false
+        });
     }
 }
