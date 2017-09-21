0dd9fe8bad7e4c34941472c411814cc07bd0d0f5
kumar m <cmynam@gmail.com>
2017-09-21 11:47:20 -0600

    Initial commit.

--- app/config/environment.ts
+++ app/config/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
 
-    ldapServerUrl : "ldap://ldapdevvip.co.ihc.com",
-    commonDummyNum : 1,
-    commonDummyBool : false,
-    commonDummyDate : new Date(2016, 12, 31),
+    ldapServerUrl: "ldap://ldapdevvip.co.ihc.com",
+    commonDummyNum: 1,
+    commonDummyBool: false,
+    commonDummyDate: new Date(2016, 12, 31),
 
 }
