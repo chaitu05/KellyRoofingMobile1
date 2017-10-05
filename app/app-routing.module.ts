378b3378e01bd830e37054b8e617bfdbacdcfdfa
kumar m <cmynam@gmail.com>
2017-10-05 00:17:57 -0600

    profile component, module created, and loaded into app.module and wired into side drawer.

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { NativeScriptRouterModule } from "nativescript-angular/router";
 const routes: Routes = [
     { path: "", redirectTo: "/orders-list", pathMatch: "full" },
     { path: "tabs", loadChildren: "./tabs/tabs.module#TabsModule" },
-    { path: "orders-list",
-        loadChildren: "./pages/orders-list/orders-list.module#OrdersListModule" }
+    { path: "orders-list", loadChildren: "./pages/orders-list/orders-list.module#OrdersListModule" },
+    { path: "profile", loadChildren: "./pages/profile/profile.module#ProfileModule" }
 ];
 
 
