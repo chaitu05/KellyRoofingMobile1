060323ddbd91d97ab39c8081cfd3b66a60bee591
kumar m <cmynam@gmail.com>
2017-10-11 14:00:41 -0600

    Orders Layout updated.

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -3,9 +3,8 @@ import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/scratch", pathMatch: "full" },
-    // { path: "", redirectTo: "/orders-list", pathMatch: "full" },
-    // { path: "", redirectTo: "/tabs", pathMatch: "full" },
+    // { path: "", redirectTo: "/scratch", pathMatch: "full" },
+    { path: "", redirectTo: "/orders-list", pathMatch: "full" },
     { path: "orders-list", loadChildren: "./pages/orders-list/orders-list.module#OrdersListModule" },
     { path: "scratch", loadChildren: "./pages/scratch-comp/scratch.module#ScratchModule" },
     { path: "profile", loadChildren: "./pages/profile/profile.module#ProfileModule" }
